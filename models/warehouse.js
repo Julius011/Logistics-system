5b31686ae6ff6cfcb8a2a49b6a8c371b9784fa6c
Julius011 <rosengren.julius@elev.ga.ntig.se>
2023-12-05 10:05:45 +0100

    Updated wearehouse schema

--- models/warehouse.js
+++ models/warehouse.js
@@ -1,12 +1,31 @@
 import { Schema, model } from 'mongoose';
 
 const warehouseSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   name: {
     type: String,
     required: true,
     unique: true,
-  }
-  // Other warehouse details
+  },
+  addres: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  products: [{
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product',
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+  }],
 });
 
 export const Warehouse = model('Warehouse', warehouseSchema);
