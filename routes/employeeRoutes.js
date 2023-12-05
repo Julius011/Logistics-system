d98270b0b205191ac43d9a5a3179e438b838a288
Julius011 <rosengren.julius@elev.ga.ntig.se>
2023-12-05 10:25:37 +0100

    created employeeRoute

---
+++ routes/employeeRoutes.js
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+const router = Router();
+import { Employee } from '../models/employee.js'; // Assuming Product model file path
+
+// GET all employess
+router.get('/', async (req, res) => {
+    try {
+      const employee = await Employee.find();
+      res.json(employee);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// GET product by ID
+router.get('/:id', async (req, res) => {
+    try {
+      const employee = await Employee.findById(req.params.id);
+      if (!employee) {
+        return res.status(404).json({ error: 'Employee not found' });
+      }
+      res.json(employee);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Other employee-related routes (POST, PUT, DELETE, etc.)
+
+export default router;
