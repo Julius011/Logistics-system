2d71a8a460191d2520a5df99578b006736687e8e
Julius011 <rosengren.julius@elev.ga.ntig.se>
2023-12-19 10:27:40 +0100

    Updated structure

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -2,66 +2,75 @@ import { Router } from 'express';
 import { Employee } from '../models/employee.js';
 const router = Router();
 
+// Utility function to get a random number in interval
 const rndNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
+// Utility function to get a random item from an array
+const getRandomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// Utility function to generate random employee schedule
+const generateRandomSchedule = (daysData) => ({
+  dayOfWeek: getRandomItem(daysData),
+  workHours: rndNum(6, 12),
+});
+
+// Utility function to generate random employee
+const generateRandomEmployee = (id, namesData, daysData) => {
+  const randomName = getRandomItem(namesData);
+  const randomRole = Math.random() < 0.5 ? 'driver' : 'picker';
+
+  return {
+    id: id.toString(),
+    name: randomName,
+    role: randomRole,
+    schedules: [generateRandomSchedule(daysData)],
+  };
+};
+
 // Create new employees
-// createEmployee();
-async function createEmployee() {
+// createEmployee(100);
+async function createEmployee(numEmployees) {
   try {
-    Employee.collection.drop()
-    const namesData = (await (Bun.file("data/nameData.txt").text())).split("\n");
-    const daysData  = (await (Bun.file("data/dateData.txt").text())).split("\n");
-
-    // Utility function to get a random item from an array
-    const getRandomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
-
-    // Utility function to generate random employee schedule
-    const generateRandomSchedule = () => ({
-      dayOfWeek: getRandomItem(daysData),
-      workHours: rndNum(6, 12),
-    });
-
-    // Utility function to generate random employee
-    const generateRandomEmployee = (id) => {
-      const randomName = getRandomItem(namesData);
-      const randomRole = Math.random() < 0.5 ? 'driver' : 'picker';
-
-      return {
-        id: id.toString(),
-        name: randomName,
-        role: randomRole,
-        schedules: [generateRandomSchedule()],
-      };
-    };
-
-    // Change x in i < x to the number of employees you want to create
-    for (let i = 0; i < 100; i++) {
-      const employeeData = generateRandomEmployee(i);
-      const employee = await Employee.create(employeeData);
-      console.log(`Created employee: ${employee.name}`);
-    };
+    await Employee.collection.drop()
   } catch (error) {
-    console.log(error);
+    console.log(`Failed dropping Employee database: ${error.message}`);
   }
+
+  const namesData = (await (Bun.file("data/nameData.txt").text())).split("\n");
+  const daysData = (await (Bun.file("data/dateData.txt").text())).split("\n");
+
+  // Change x in i < x to the number of employees you want to create
+  for (let i = 0; i < numEmployees; i++) {
+    const employeeData = generateRandomEmployee(i, namesData, daysData);
+
+    let employee;
+    try {
+      employee = await Employee.create(employeeData);
+    } catch (error) {
+      console.log(`Could not create employee ${i}: ${error.message}`);
+    }
+
+    console.log(`Created employee: ${employee.name}`);
+  };
 }
 
 // GET all employess
 router.get('/', async (req, res) => {
-    try {
-      const employee = await Employee.find();
-      res.json(employee);
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const employee = await Employee.find();
+    res.json(employee);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // GET driver or picker working on a specific day
-router.get('/:type/:day', async ({params: {type: employeeType, day: wantedDay}}, res) => {
+router.get('/:type/:day', async ({ params: { type: employeeType, day: wantedDay } }, res) => {
   try {
     if (wantedDay === "Today") {
       wantedDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
     }
-    
+
     const driversFriday = await Employee.find({
       'role': employeeType,
       'schedules.dayOfWeek': wantedDay,
